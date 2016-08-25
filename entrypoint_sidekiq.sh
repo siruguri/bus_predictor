2ce1223d329ee4ef9ef9b3a659b7d1b966fb8025
Sameer Siruguri <siruguri@gmail.com>
2016-08-25 15:23:40 -0700

    Commit IV.b

--- entrypoint_sidekiq.sh
+++ entrypoint_sidekiq.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-apt-get -y install nodejs
-export REDIS_HOST=redis
 bundle exec sidekiq
--- entrypoint_webapp.sh
+++ entrypoint_webapp.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-apt-get -y install nodejs
-bundle exec rails s -p 5000 -b '0.0.0.0'
+rm -f tmp/pids/*pid && bundle exec rails s -p 5000 -b '0.0.0.0'
